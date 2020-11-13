fef00d2c506e65afdfd620536bf0fbbd071017c5
brendan richards <brendan_richards_msdn@hotmail.com>
2020-11-13 14:09:07 +1000

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CompanyEditComponent } from './company/company-edit/company-edit.component';
+import { CompanyListComponent } from './company/company-list/company-list.component';
+import { CompanyTableComponent } from './company/company-table/company-table.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'company/list', pathMatch: 'full'},
+  { path: 'company/list', component: CompanyListComponent },
+  { path: 'company/new', component: CompanyEditComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
