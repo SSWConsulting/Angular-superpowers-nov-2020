3442a7277697539fd6526ffcf51c2b18f1b33b07
JeanThirion <jean.thirion@esial.net>
2020-11-13 16:19:57 +1100

    implemented Save/Edit

--- src/app/company/company-edit/company-edit.component.ts
+++ src/app/company/company-edit/company-edit.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Company } from '../company';
+import { CompanyService } from '../company.service';
 
 @Component({
   selector: 'app-company-edit',
@@ -7,9 +11,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CompanyEditComponent implements OnInit {
 
-  constructor() { }
+  isNewCompany: boolean;
+  companyId: number;
+  companyForm: FormGroup;
+
+  constructor(
+    private companyService: CompanyService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.companyId = this.activatedRoute.snapshot.params['id'];
+    this.isNewCompany = !this.companyId;
+    this.buildForm();
+
+    if (!this.isNewCompany) {
+      this.companyService.getCompany(this.companyId)
+      .subscribe(c => this.companyForm.patchValue(c));
+    }
+  }
+
+  buildForm(): void {
+    // this.companyForm = new FormGroup({
+    //   name: new FormControl(),
+    //   phone: new FormControl(),
+    //   email: new FormControl()
+    // });
+
+    this.companyForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      phone: [''],
+      email: ['Test@ssw.com.au']
+    });
+  }
+
+  saveCompany(): void {
+
+    if (this.isNewCompany) {
+      const company: Company = this.companyForm.value;
+      this.companyService.addCompany(company)
+      .subscribe((c: Company) => this.router.navigateByUrl('/company/list'));
+
+    }else{
+      // let c: Company = {
+      //   id: this.companyId,
+      //   name: this.companyForm.get('name').value,
+      //   phone: this.companyForm.get('phone').value,
+      //   email: this.companyForm.get('email').value
+      // };
+
+      const c: Company = {...this.companyForm.value, id: this.companyId};
+
+      this.companyService.updateCompany(c)
+      .subscribe((c: Company) => this.router.navigateByUrl('/company/list'));
+    }
+
+
   }
 
 }
--- src/app/company/company.service.ts
+++ src/app/company/company.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of} from 'rxjs';
 import { Company } from './company';
@@ -26,6 +26,30 @@ export class CompanyService {
   deleteCompany(company: Company): Observable<Company>{
     console.log('SERVICE - delete company called', company);
     return this.httpClient.delete<Company>(`${this.API_BASE}/company/${company.id}`)
+    .pipe(
+      catchError(this.errorHandler)
+    );
+  }
+
+  addCompany(company: Company): Observable<Company>{
+    return this.httpClient.post<Company>(`${this.API_BASE}/company`, company,
+    { headers: new HttpHeaders().set('content-type', 'application/json')})
+    .pipe(
+      catchError(this.errorHandler)
+    );
+  }
+
+  getCompany(companyId: number): Observable<Company> {
+    return this.httpClient.get<Company>(`${this.API_BASE}/company/${companyId}`)
+    .pipe(
+      catchError(this.errorHandler)
+    );
+  }
+
+  updateCompany(company: Company): Observable<Company> {
+    return this.httpClient.put<Company>(`${this.API_BASE}/company/${company.id}`,
+    company,
+    { headers: new HttpHeaders().set('content-type', 'application/json')})
     .pipe(
       catchError(this.errorHandler)
     );
